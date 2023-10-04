ef6f878ed58edb8d1f75436700c912e81b0ca8c7
Michelle Mabuyo <michelle@apollographql.com>
2023-10-04 14:18:35 -0400

    remove old TS code

--- typescript/subgraph-quiz/resolvers/Leaderboard.ts
+++
@@ -1,8 +0,0 @@
-export const Leaderboard = {
-  Leaderboard: {
-    allQuizzes(_parent, _args, _context) {
-      console.log("all quizzes");
-      return [{ id: "11" }, { id: "22" }, { id: "33" }];
-    },
-  },
-};
